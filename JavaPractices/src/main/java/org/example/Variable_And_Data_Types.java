e67729b5aca5344f8687798dfc6582798e4d23e7
Joy baidya <joybaidya1137@gmail.com>
2025-05-23 12:52:20 +0600

    commenting java practice

---
+++ JavaPractices/src/main/java/org/example/Variable_And_Data_Types.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Variable_And_Data_Types {
+    public static void main(String[] args) {
+        int A = 20;
+        char C = 'j';
+        boolean Y = true;
+        float F = 1.2f;
+        double B = 3.23;
+
+
+        System.out.println("integer number A: "+A);
+        System.out.println("Char C:"+C);
+        System.out.println("Boolean Y:"+Y);
+        System.out.println("Float F:"+F);
+        System.out.println("Double B ="+B);
+
+    }
+}
